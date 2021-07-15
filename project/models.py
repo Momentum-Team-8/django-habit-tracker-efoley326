849f7fb00779d74b5f125938a4e15f3a2e681de6
Emily Foley <efoley326@gmail.com>
2021-07-14 22:00:00 -0400

    created date

--- project/models.py
+++ project/models.py
@@ -16,6 +16,7 @@ class HabitEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     habit = models.CharField(max_length=255)
     amount = models.CharField(max_length=255, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
         return {self.habit}
